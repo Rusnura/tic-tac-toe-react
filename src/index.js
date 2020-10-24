dcdc2c99d2f4364b08fd06489e42e9fa7753767f
Ruslan Tumasov <Rus-nura@yandex.ru>
2020-10-24 18:59:24 +0700

    refactor & added winner calculation

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 
 function Square(props) {
   return (
-    <button className="square" onClick={props.onClickFx}>
+    <button className="square" onClick={props.onClick}>
       {props.value}
     </button>
   );
@@ -21,16 +21,23 @@ class Board extends React.Component {
 	
 	this.handleClick = function(i) {
 	  const squares = this.state.squares.slice();
+	  if (calculateWinner(squares) || squares[i]) return;
 	  squares[i] = this.state.isNextX ? 'X' : 'O';
 	  this.setState({squares: squares, isNextX: !this.state.isNextX});
 	}
   }
   renderSquare(i) {
-    return <Square value={this.state.squares[i]} onClickFx={() => {this.handleClick(i)}} />;
+    return <Square value={this.state.squares[i]} onClick={() => {this.handleClick(i)}} />;
   }
 
   render() {
-    const status = 'Next player: X';
+	const winner = calculateWinner(this.state.squares);
+	let status;
+    if (winner) {
+      status = 'Выиграл ' + winner;
+    } else {
+      status = 'Следующий ход: ' + (this.state.xIsNext ? 'X' : 'O');
+    }
 
     return (
       <div>
@@ -77,3 +84,23 @@ ReactDOM.render(
   <Game />,
   document.getElementById('root')
 );
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
